9c7b2de87368e674f1e54148f1a59aa6594cf617
MariaSosa9722 <mariasosa9722@gmail.com>
2025-08-25 19:33:22 -0500

    Agregar archivos de proyecto.

---
+++ 05-For-Foreach/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+using _05_For_Foreach;
+
+Console.WriteLine("Hello, World!");
+
+
+
+string[] frutas = { "Manzana", "Banana", "Naranja", "Pera" };
+int[] Numeros = { 1, 2,3, 4 };
+
+
+//Console.WriteLine("Estoy fuera del for");
+//for (int i = 0; i < frutas.Length; i++)
+//{
+//    Console.WriteLine($"Indice de frutas {i}: { frutas[i] }");
+//}
+
+//for (int i = 0; i < Numeros.Length; i++)
+//{
+//    Console.WriteLine($"Indice de números {i}: {Numeros[i]}");
+//}
+
+//foreach (string i in frutas)
+//{
+//    Console.WriteLine(i);
+
+//}
+
+
+
+//foreach (int item in Numeros)
+//{
+//    Console.WriteLine(item);
+
+//}
+
+
+ejercicio ejercicio = new ejercicio();
+ejercicio.inicio();
+
+
---
+++ 05-For-Foreach/ejercicio.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace _05_For_Foreach
+{
+    public class ejercicio
+    {
+        public void inicio()
+        {
+
+            Console.WriteLine("Cuantos numeros deseas capturar");
+            int cantidad = int.Parse(Console.ReadLine());
+
+            // declaramos los numeros en el arreglo
+            int[] numeros = new int[cantidad];
+
+            for (int i = 0; i < numeros.Length; i++) 
+            {
+
+                Console.WriteLine($"Ingresa el número {i+1} : ");
+                numeros[i] = int.Parse(Console.ReadLine());
+            
+            }
+
+            int sumarFor = 0;
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                sumarFor += numeros[i];
+                
+
+            }
+
+            Console.WriteLine($"La suma en for es :"+sumarFor);
+        
+        }
+
+    }
+}
